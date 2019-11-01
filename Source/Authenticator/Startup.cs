9c8d80560f310b07d4e09b6a7310740c239e4b9e
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-01 10:57:22 +0100

    Skeleton for Authenticator server

---
+++ Source/Authenticator/Startup.cs
@@ -0,0 +1,43 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using Autofac;
+using Dolittle.Booting;
+using Dolittle.DependencyInversion.Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Authenticator
+{
+    public partial class Startup
+    {
+        readonly IHostingEnvironment _hostingEnvironment;
+        readonly ILoggerFactory _loggerFactory;
+        BootloaderResult _bootResult;
+
+        public Startup(IHostingEnvironment hostingEnvironment, ILoggerFactory loggerFactory)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _loggerFactory = loggerFactory;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            _bootResult = services.AddDolittle(_loggerFactory);
+        }
+
+        public void ConfigureContainer(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.AddDolittle(_bootResult.Assemblies, _bootResult.Bindings);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
