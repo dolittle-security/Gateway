107ef614cde15efe1dac465a88d4ed69eb5c98c0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-06 10:51:38 +0100

    Fix namespace

--- Source/Core/Program.cs
+++ Source/Core/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Autofac.Extensions.DependencyInjection;
 
-namespace Signin
+namespace Core
 {
     public class Program
     {
--- Source/Core/Startup.cs
+++ Source/Core/Startup.cs
@@ -16,7 +16,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Signin
+namespace Core
 {
     public partial class Startup
     {
