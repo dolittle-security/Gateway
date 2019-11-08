9818820c375c21fa3081975a889acd01928fc583
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-08 07:45:19 +0100

    Added Concepts

---
+++ Source/Concepts/PortalId.cs
@@ -0,0 +1,15 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using System;
+using Dolittle.Concepts;
+
+namespace Concepts
+{
+    public class PortalId : ConceptAs<Guid>
+    {
+        public static implicit operator PortalId(Guid id) => new PortalId { Value = id };
+    }
+}
