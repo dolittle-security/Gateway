6d022eec985a890a5609079532da9f1712b0bac0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-12 13:31:51 +0100

    Error reporting for exceptions in boot procedure tasks

--- Source/Providers/Dynamic/Boot.cs
+++ Source/Providers/Dynamic/Boot.cs
@@ -3,11 +3,13 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using System;
 using System.Threading.Tasks;
 using Dolittle.Booting;
 using Dolittle.Collections;
 using Dolittle.DependencyInversion;
 using Dolittle.Execution;
+using Dolittle.Logging;
 using Dolittle.Runtime.Tenancy;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Read.Providers.Configuring;
@@ -19,12 +21,14 @@ namespace Providers.Dynamic
         readonly IExecutionContextManager _contextManager;
         readonly ITenants _tenants;
         readonly FactoryFor<ConfigurationManager> _configurationManagerFactory;
+        readonly ILogger _logger;
 
-        public Boot(IExecutionContextManager contextManager, ITenants tenants, FactoryFor<ConfigurationManager> configurationManagerFactory)
+        public Boot(IExecutionContextManager contextManager, ITenants tenants, FactoryFor<ConfigurationManager> configurationManagerFactory, ILogger logger)
         {
             _contextManager = contextManager;
             _tenants = tenants;
             _configurationManagerFactory = configurationManagerFactory;
+            _logger = logger;
         }
 
         public bool CanPerform() => true;
@@ -33,8 +37,15 @@ namespace Providers.Dynamic
         {
             _tenants.All.ForEach(tenant => {
                 Task.Factory.StartNew(() => {
-                    _contextManager.CurrentFor(tenant);
-                    _configurationManagerFactory().Start();
+                    try
+                    {
+                        _contextManager.CurrentFor(tenant);
+                        _configurationManagerFactory().Start();
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.Error(e, $"Error while startig identity provider configuration manager for tenant '{tenant}'. No providers will be available for that tenant.");
+                    }
                 });
             });
         }
