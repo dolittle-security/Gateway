bce821741b7577ea2083a17ca49290264caab744
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-22 09:02:12 +0100

    Add AuthorizeDeviceWithUserCode command

---
+++ Source/Domain/Clients/Device/AuthorizeDeviceWithUserCode.cs
@@ -0,0 +1,14 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Dolittle.Commands;
+
+namespace Domain.Clients.Device
+{
+    public class AuthorizeDeviceWithUserCode : ICommand
+    {
+        public string UserCode { get; set; }
+    }
+}
---
+++ Source/Web/Features/Clients/Device/AuthorizeDeviceWithUserCode.js
@@ -0,0 +1,15 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class AuthorizeDeviceWithUserCode extends Command
+{
+    constructor() {
+        super();
+        this.type = '0c5a9410-5c55-452a-a95b-a1e6ddc2d003';
+
+        this.userCode = '';
+    }
+}
