cd72cb33212045c5c89381ba22cdf6f1b4a0eedc
Vidar Aune Westrum <vidar@dolittle.com>
2019-11-27 09:43:58 +0100

    update to latest webpack package

--- Source/Web/webpack.config.js
+++ Source/Web/webpack.config.js
@@ -2,22 +2,23 @@ const path = require('path');
 require('dotenv').config();
 const process = require('process');
 
-const webpack = require('@dolittle/typescript.webpack.aurelia').webpack
+const webpack = require('@dolittle/typescript.webpack.aurelia').webpack;
 const upstreamConfig = webpack(__dirname);
 
-module.exports = (environment) => {
+module.exports = environment => {
     if (environment && environment.production) {
-        process.env.DOLITTLE_WEBPACK_BASE_URL = '/signin/'
+        process.env.DOLITTLE_WEBPACK_BASE_URL = '/signin/';
     }
     const config = upstreamConfig.apply(null, arguments);
     config.resolve.alias.DolittleStyles = path.resolve(__dirname, './Styles');
     config.module.rules.unshift({
         test: /environment\.json$/i,
-        use: [{
-            loader: 'app-settings-loader',
-            options: { env: environment && environment.production ? 'production' : 'development' },
-        }]
+        use: [
+            {
+                loader: 'app-settings-loader',
+                options: { env: environment && environment.production ? 'production' : 'development' }
+            }
+        ]
     });
     return config;
 };
-
