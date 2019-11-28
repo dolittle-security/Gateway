42e7afc3a9fa06b22af957f1ad3bb6fcaabf7ef0
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-28 10:32:39 +0100

    Fix serving of non-text files

--- Source/Authentication/Frontend/CustomFrontendServer.cs
+++ Source/Authentication/Frontend/CustomFrontendServer.cs
@@ -3,37 +3,41 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using System.IO;
 using Context;
+using Dolittle.Execution;
 using Dolittle.IO;
 using Dolittle.IO.Tenants;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace Authentication.Frontend
 {
     public class CustomFrontendServer : ICustomFrontendServer
     {
+        const string _basePath = "./wwwroot";
+
         readonly IPortalContextManager _portalManager;
         readonly IFileSystem _sharedFiles;
         readonly IFiles _tenantFiles;
+        readonly FileExtensionContentTypeProvider _contentTypeProvider;
+        readonly IExecutionContextManager _executionContextManager;
 
-        public CustomFrontendServer(IPortalContextManager portalManager, IFileSystem sharedFiles, IFiles tenantFiles)
+        public CustomFrontendServer(IPortalContextManager portalManager, IFileSystem sharedFiles, IFiles tenantFiles, IExecutionContextManager executionContextManager)
         {
             _portalManager = portalManager;
             _sharedFiles = sharedFiles;
             _tenantFiles = tenantFiles;
+            _contentTypeProvider = new FileExtensionContentTypeProvider();
+            _executionContextManager = executionContextManager;
         }
 
         public IActionResult ServeFile(string file)
         {
-            if (TryGetCustomOrDefaultFile(file, out var contents))
+            if (TryGetCustomOrDefaultFile(file, out var contents, out var mimetype))
             {
-                return new ContentResult
-                {
-                    StatusCode = StatusCodes.Status200OK,
-                    ContentType = "text/html; charset=UTF-8",
-                    Content = contents,
-                };
+                return new FileContentResult(contents, mimetype);
             }
             else
             {
@@ -41,31 +45,49 @@ namespace Authentication.Frontend
             }
         }
 
-        bool TryGetCustomOrDefaultFile(string file, out string contents)
+        bool TryGetCustomOrDefaultFile(string file, out byte[] contents, out string mimetype)
         {
-            contents = "";
-            var portalId = _portalManager.Current.Portal;
-            if (_tenantFiles.DirectoryExists($"{portalId}"))
+            var customDirectoryPath = GetTenantPortalPath();
+            if (Directory.Exists(customDirectoryPath))
             {
-                try
-                {
-                    contents = _tenantFiles.ReadAllText($"{portalId}/{file}");
-                    return true;
-                }
-                catch
-                {
-                    return false;
-                }
+                return TryReadFileContents(customDirectoryPath, file, out contents, out mimetype);
             }
+            else
+            {
+                var defaultDirectoryPath = Path.Combine(_basePath, "default");
+                return TryReadFileContents(defaultDirectoryPath, file, out contents, out mimetype);
+            }
+        }
+
+        bool TryReadFileContents(string @base, string file, out byte[] contents, out string mimetype)
+        {
+            mimetype = "";
+            contents = null;
+
+            // FIXME: Ensure that we don't escape the sandbox here...
+
+            if (!_contentTypeProvider.TryGetContentType(file, out mimetype))
+            {
+                return false;
+            }
+
+            var path = Path.Combine(@base, file);
             try
             {
-                contents = _sharedFiles.ReadAllText($"./wwwroot/default/{file}");
+                contents = File.ReadAllBytes(path);
                 return true;
             }
             catch
             {
                 return false;
             }
+        }
+
+        string GetTenantPortalPath()
+        {
+            var tenantPath = _executionContextManager.Current.Tenant.ToString();
+            var portalPath = _portalManager.Current.Portal.ToString();
+            return Path.Combine(_basePath, tenantPath, portalPath);
         }
     }
 }
