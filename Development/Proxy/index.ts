a4a14a5c7f68498d5dab2dc71dfb4d8280db11ec
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-11-28 10:33:58 +0100

    Fix development proxy

--- Development/Proxy/index.ts
+++ Development/Proxy/index.ts
@@ -1,58 +1,30 @@
 import * as express from 'express';
 import * as http from 'http';
+import { ClientRequest, IncomingMessage } from 'http';
 import * as querystring from 'querystring';
+import * as proxy from 'http-proxy-middleware';
 import { Express, Request, Response } from 'express';
 import { Configuration, Portal } from "../Configuration";
-import { ParamsDictionary } from 'express-serve-static-core';
 
-async function PrepareRequestToGateway(req: Request<ParamsDictionary>, res: Response, portal: Portal, path: string, responseCallback: (response: http.IncomingMessage) => void): Promise<http.ClientRequest>
+function FindPortalFromRequest(req: Request, configuration: Configuration): Portal
 {
-    const headers = req.headers;
-    headers['Owner-Tenant-ID'] = portal.Owner.toString();
-    headers['Portal-ID'] = portal.Id.toString();
-    headers['Portal-Domain'] = portal.Domain;
-    headers['X-Forwarded-Host'] = req.headers.host;
-
-    const query = querystring.stringify(req.query);
-    const proxyRequest = http.request({
-        host: 'localhost',
-        port: 5000,
-        path: path + (query ? '?'+query : ''),
-        method: req.method,
-        headers: headers,
-    }, responseCallback).on('error', (error) => {
-        console.log(`Error forwarding request to proxy ${error.message}`);
-        res.status(500);
-        res.end();
-    });
-    return proxyRequest;
+    for (const portal of configuration.Portals)
+    {
+        if (req.hostname.endsWith(portal.Domain))
+        {
+            return portal;
+        }
+    }
+    throw `No portal found for host ${req.hostname}`;
 }
 
-async function ForwardRequestToGateway(req: Request<ParamsDictionary>, res: Response, portal: Portal, rewritePath: (path: string) => string)
+function AddHeadersToProxyRequest(req: Request, proxyReq: ClientRequest, configuration: Configuration): void
 {
-    const proxyRequest = await PrepareRequestToGateway(req, res, portal, rewritePath(req.path), (proxyResponse) => {
-        const chunks = [];
-        proxyResponse.on('data', (chunk) => {
-            chunks.push(chunk);
-        });
-        proxyResponse.on('end', () => {
-            const data = Buffer.concat(chunks, parseInt(proxyResponse.headers["content-length"]));
-            res.set(proxyResponse.headers);
-            res.status(proxyResponse.statusCode);
-            res.end(data);
-        });
-        proxyResponse.on('error', (error) => {
-            console.log(`Error forwarding request to proxy ${error.message}`);
-            res.status(500);
-            res.end();
-        });
-    });
-    req.on('data', (chunk) => {
-        proxyRequest.write(chunk);
-    });
-    req.on('end', () => {
-        proxyRequest.end();
-    });
+    const portal = FindPortalFromRequest(req, configuration);
+    proxyReq.setHeader('Owner-Tenant-ID', portal.Owner.toString());
+    proxyReq.setHeader('Portal-ID', portal.Id.toString());
+    proxyReq.setHeader('Portal-Domain', portal.Domain);
+    proxyReq.setHeader('X-Forwarded-Host', req.headers.host);
 }
 
 class AuthSubrequestResult
@@ -62,7 +34,7 @@ class AuthSubrequestResult
     public Claims: any;
 }
 
-function GetClaims(response: http.IncomingMessage): any
+function GetClaims(response: IncomingMessage): any
 {
     const claims = {};
     const claimsHeader = response.headers['claims'] as string;
@@ -73,68 +45,90 @@ function GetClaims(response: http.IncomingMessage): any
     return claims;
 }
 
-async function PerformAuthSubRequest(req: Request<ParamsDictionary>, res: Response, portal: Portal): Promise<AuthSubrequestResult>
+async function PerformAuthSubrequest(req: Request, configuration: Configuration): Promise<AuthSubrequestResult>
 {
-    return new Promise(async (resolve) => {
-        console.log(`Performing auth subrequest`);
-        const proxyRequest = await PrepareRequestToGateway(req, res, portal, '/auth', (proxyResponse) => {
-            resolve({
-                StatusCode: proxyResponse.statusCode,
-                TenantID: proxyResponse.headers['tenant-id'] as string,
-                Claims: GetClaims(proxyResponse),
-            });
+    return new Promise<AuthSubrequestResult>((resolve, reject) => {
+        const authRequest = http.request({
+            hostname: 'localhost',
+            port: 5000,
+            path: '/auth',
+            method: 'GET',
+            headers: req.headers,
         });
-        proxyRequest.end();
-    });
-
+        AddHeadersToProxyRequest(req, authRequest, configuration);
+        authRequest.on('response', (authResponse) => {
+            const result: AuthSubrequestResult =
+            {
+                StatusCode: authResponse.statusCode,
+                TenantID: undefined,
+                Claims: undefined,
+            };
+            if (authResponse.statusCode == 200)
+            {
+                result.TenantID = authResponse.headers['tenant-id'] as string;
+                result.Claims = GetClaims(authResponse);
+            }
+            resolve(result);
+        });
+        authRequest.on('error', (error) => {
+            reject(error);
+        });
+        authRequest.end();
+    })
 }
 
-async function HandlePortalRequest(req: Request<ParamsDictionary>, res: Response, portal: Portal)
+function CreateNormalPageHandler(configuration: Configuration): (req: Request, res: Response) => void
 {
-    if (req.path.startsWith('/.well-known') || req.path.startsWith('/connect') || req.path.startsWith('/signin') || req.path.startsWith('signout') || req.path.startsWith('/dolittle/security/gateway'))
-    {
-        await ForwardRequestToGateway(req, res, portal, (path) => path);
-    }
-    else if (req.path.startsWith('/api/dolittle/security/gateway'))
-    {
-        await ForwardRequestToGateway(req, res, portal, (path) => path.replace('/api/dolittle/security/gateway', '/api'));
-    }
-    else
-    {
-        const authResponse = await PerformAuthSubRequest(req, res, portal);
+    return async (req, res) => {
+        const authResponse = await PerformAuthSubrequest(req, configuration);
+        console.log('AuthResponse status', authResponse.StatusCode);
         if (authResponse.StatusCode == 401 || authResponse.StatusCode == 403)
         {
-            res.redirect(302, `/signin?rd=${encodeURIComponent(req.path)}`);
-            res.end();
+            let redirectUri = req.path;
+            let redirectQuery = querystring.stringify(req.params);
+            if (redirectQuery)
+            {
+                redirectUri += '?'+redirectQuery;
+            }
+            res.redirect(`/signin?rd=${encodeURIComponent(redirectUri)}`)
         }
         else
         {
+            const portal = FindPortalFromRequest(req, configuration);
             res.status(200);
             res.write(`Welcome to portal ${portal.DisplayName}\n`);
             res.write(`You are signed in to tenant ${authResponse.TenantID}\n`);
             res.write(`Your claims are:\n`);
             res.end(JSON.stringify(authResponse.Claims));
         }
-    }
+    };
 }
 
 export function CreateProxy(configuration: Configuration): Express
 {
     const server = express();
 
-    for (const portal of configuration.Portals) {
-        server.use(async (req, res, next) => {
-            if (req.hostname.endsWith(portal.Domain))
-            {
-                console.log(`Handling request for portal ${portal.DisplayName} to '${req.path}'`);
-                await HandlePortalRequest(req, res, portal);
-            }
-            else
-            {
-                next();
-            }
-        });
-    }    
+    const forwarder = proxy({
+        target: 'http://localhost:5000',
+        ws: true,
+        pathRewrite: {
+            '^/api/dolittle/security/gateway': '/api',
+        },
+        onProxyReq: (proxyReq: ClientRequest, req: Request) => {
+            console.log('Forwarding for', req.path);
+            AddHeadersToProxyRequest(req, proxyReq, configuration);
+        },
+    });
+
+    const handler = CreateNormalPageHandler(configuration);
     
+    server.use('/signin', forwarder);
+    server.use('/signout', forwarder);
+    server.use('/dolittle/security/gateway', forwarder);
+    server.use('/api/dolittle/security/gateway', forwarder);
+    server.use('/.well-known', forwarder);
+    server.use('/connect', forwarder);
+    server.use(handler);
+
     return server;
 }
